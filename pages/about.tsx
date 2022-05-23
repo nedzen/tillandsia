9dc5d082bcb2d7e9db8a10a7ef0a8e009957f39c
marius <nedzen.11@gmail.com>
2022-05-23 23:51:18 +0200

    bug

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,7 +5,7 @@ export const About = (): JSX.Element => {
   return (
     <Layout
       customMeta={{
-        title: 'About - Hunter Chang',
+        title: 'Marius',
       }}
     >
       <h1>About Page</h1>
