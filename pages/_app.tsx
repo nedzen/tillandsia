44bb3d0b68cada9358a16dee6752899d21205b70
marius <nedzen.11@gmail.com>
2023-02-22 14:43:06 +0100

    ph

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,13 +6,22 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import posthog from 'posthog-js';
 
+// if (typeof window !== 'undefined') {
+//   // This ensures that as long as we are client-side, posthog is always ready
+//   posthog.init('phc_1QBmle4Cc7P82mKBGL3JtPpfElYMu1Eu8d4U1VcwQ4v', {
+//     api_host: 'https://app.posthog.com',
+//     loaded: (posthog) => {
+//       if (process.env.NODE_ENV === 'development') posthog.opt_out_capturing();
+//     },
+//   });
+// }
 if (typeof window !== 'undefined') {
   // This ensures that as long as we are client-side, posthog is always ready
+  // NOTE: If set as an environment variable be sure to prefix with `NEXT_PUBLIC_`
+  // For more info see https://nextjs.org/docs/basic-features/environment-variables#exposing-environment-variables-to-the-browser
+
   posthog.init('phc_1QBmle4Cc7P82mKBGL3JtPpfElYMu1Eu8d4U1VcwQ4v', {
     api_host: 'https://app.posthog.com',
-    loaded: (posthog) => {
-      if (process.env.NODE_ENV === 'development') posthog.opt_out_capturing();
-    },
   });
 }
 
